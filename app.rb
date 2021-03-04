051ecc4b8e76569181ba1a6625cd17d9e8c4186d
Edvin-stahl-nti-johanneberg <60431172+Edvin-stahl-nti-johanneberg@users.noreply.github.com>
2021-03-04 16:23:45 +0100

    crud

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'slim'
+require_relative 
+
+
+enable :sessions
+
+
+get('')
---
+++ model.rb
@@ -0,0 +1,13 @@
+require 'sqlite3'
+require 'bcrypt'
+
+#/gör om koden till hash 
+def connect()
+    db = SQLite3::Database.new ('db/db.db ')
+    db.results_as_hash = true
+    return db
+end
+
+def varukorg()
+    varukorg = connect().execute("INSERT INTO")
+    
