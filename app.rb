911b0f861cd8cd0967868814766933635835721d
Edvin-stahl-nti-johanneberg <60431172+Edvin-stahl-nti-johanneberg@users.noreply.github.com>
2021-03-11 15:04:37 +0100

    2021-03-11

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
 require 'sinatra'
 require 'slim'
+require 'sqlite3'
+require 'bcrypt'
 require_relative 
 
 
 enable :sessions
 
-
-get('')
+get('/') do
+    slim(:register)
+  end
+  
+  get('/showlogin') do
+    slim(:login)
+  end
+  
+  post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new ('db/todo2021.db ')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id 
+      redirect('/todos')
+    else
+      "fel"
+    end
+  end 
+  
+  get('/todos') do
+    id = session[:id].to_i
+    db = SQLite3::Database.new ('db/todo2021.db ')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM todos WHERE user_id = ?",id).first
+    p "alla todos #{result}"
+    slim(:"todos/index",locals:{todos:result})
+  end
+  
+  
+  post("/users/new") do
+  username = params[:username]
+  password = params[:password]
+  password_comfirm = params[:password_comfirm]
+  
+    if (password == password_comfirm)
+      password_digest=BCrypt::Password.create(password)
+      db = SQLite3::Database.new ("db/todo2021.db ")
+      db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+      redirect("/")
+    else
+      "fel lösenord"
+    end
+  end
